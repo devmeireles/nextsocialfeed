79d242581190435901832f5c05ab52d4c8a1970c
Gabriel Meireles <meireleswebdesign@gmail.com>
2024-05-18 16:21:52 -0300

    feature(api-context): Add Feed API Context Adding initial feed route Adding a context to manage the feed API data

---
+++ src/context/FeedContext.tsx
@@ -0,0 +1,64 @@
+import INews from "@/types/INews";
+import React from "react";
+import {useQuery} from "@tanstack/react-query";
+import fetchField from "@/app/api/content/fetchFeed";
+
+interface FeedContextProps {
+    children: React.ReactNode;
+}
+
+interface IFeedContext {
+    news: INews[] | [];
+    isLoading: boolean;
+    isFetching: boolean;
+}
+
+const defaultState: IFeedContext = {
+    news: [],
+    isLoading: false,
+    isFetching: false
+};
+
+const FeedContext = React.createContext<IFeedContext>(defaultState);
+
+const useFeedContext = (): IFeedContext => {
+    const context = React.useContext(FeedContext);
+    if (!context) {
+        throw new Error("useFeedContext must be used within a FeedProvider");
+    }
+    return context;
+}
+
+const FeedProvider: React.FC<FeedContextProps> = ({children}) => {
+    const feedQuery = useQuery({
+        queryKey: ["country"],
+        queryFn: async () => fetchField(),
+    });
+
+    const feedContent = feedQuery.data ?? [];
+
+    const contextValues = {
+        news: feedContent,
+        isLoading: feedQuery.isLoading,
+        isFetching: feedQuery.isFetching
+    };
+
+    return (
+        <FeedContext.Provider value={contextValues}>
+            {children}
+        </FeedContext.Provider>
+    );
+}
+
+export {FeedContext, useFeedContext, FeedProvider};
+
+/**
+git commit -m "feature(api-context): Add Feed API Context
+Adding initial feed route
+Adding a context to manage the feed API data
+
+"
+
+ 
+
+*/
---
+++ src/providers/TanstackProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import React from "react";
+
+interface ITanstackProviderProps {
+    children: React.ReactNode;
+}
+
+export const TanstackProvider: React.FC<ITanstackProviderProps> = ({children}) => {
+    const queryClient = new QueryClient();
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+}
